6bf45ba6310b5df2432c06245cb52f3327988399
Ivaar <7287342+Ivaar@users.noreply.github.com>
2018-03-31 12:20:20 -0700

    Adjusted SendTarget for Ashita v3 API

--- SendTarget/SendTarget.lua
+++ SendTarget/SendTarget.lua
@@ -1,5 +1,5 @@
 --[[
-    Copyright (c) 2015, SblmS2J
+    Copyright (c) 2018, Ivaar
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -17,7 +17,7 @@
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
     ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
     WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-    DISCLAIMED. IN NO EVENT SHALL <your name> BE LIABLE FOR ANY
+    DISCLAIMED. IN NO EVENT SHALL IVAAR BE LIABLE FOR ANY
     DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
     (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
@@ -27,43 +27,39 @@
 ]]
 
 _addon.name = 'SendTarget';
-_addon.version = '1.0.0.0';
+_addon.version = '1.18.03.29';
 _addon.author = 'Ivaar';
 
 require 'common'
-require 'events'
-target = require('ffxi.target');
+require('ffxi.targets');
 
----------------------------------------------------------------------------------------------------
--- func: command
--- desc: Called when our addon receives a command.
----------------------------------------------------------------------------------------------------
 ashita.register_event('command', function(cmd, nType)
-    local args = cmd:GetArgs();
+    local args = cmd:args();
     if (args[1] ~= '/sendtarget' and args[1] ~= '/st') then
         return false;
     end
-    
+
     if (#args == 1 or args[2] == 'help') then
-        print(string.format('Sends alternate /command <last sub target ID>.\nMacro line 1: /target <st>\nMacro line 2: /st to name command '));
+        print(string.format('Sends mule /command <lastst>.\nMacro line 1: /target <st>\nMacro line 2: /st to name command '));
         return true;
     end
-    
+
     if (#args <= 3) then		
         return true;
     end
-    
+
     if (args[2] ~= 'to') then
         return true
     end
-    
+
     if (args[4]:startswith('/') == false) then
         args[4] = '/' .. args[4];
     end
-    
-    local entity = get_target('lastst');
+
+    local entity = GetEntity(ashita.ffxi.targets.get_target_lastst());
+
     if (entity ~= nil) then
-        local str = '/servo sendto '.. table.concat(args,' ',3) ..' '.. entity.ServerID;
+        local str = '/ms sendto '.. table.concat(args,' ',3) ..' '.. entity.ServerId;
         AshitaCore:GetChatManager():QueueCommand(str, -1);
     else
         print(string.format('Could not find last sub-target. No action performed...'));
